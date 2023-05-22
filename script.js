9324605572e0f156a791ec6bfe2abae454b808bb
salikwani <120441251+salikwani@users.noreply.github.com>
2023-05-22 09:58:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -50,27 +50,26 @@ function getDetails(e) {
     str = arr.join("%20");
     arr = str.split(",");
     str = arr.join("%2C");
-    
-    fetch(`https://api.geoapify.com/v1/geocode/search?text=${str}&apiKey=a9212495cb8b43b6ab6eb2d331dd1a11`).then(response => response.json())
-    .then(result => {
-        console.log(result);
-        if (result.features.length === 0) {
-            document.getElementById("address").innerHTML += "<p>Please enter a valid address</p>";
-        } else {
-            document.getElementById("address").innerHTML += `<h1>Your Result</h1>
-                                                            <div>
-                                                            <p>Name of Time Zone: ${result.features[0].properties.timezone.name}</p>
-                                                            <p>Lat: ${result.features[0].properties.lat}</p>
-                                                            <p>Long: ${result.features[0].properties.lon}</p>
-                                                            <p>Offset STD: ${result.features[0].properties.timezone.offset_STD}</p>
-                                                            <p>Offset STD Seconds: ${result.features[0].properties.timezone.offset_STD_seconds}</p>
-                                                            <p>Offset DST: ${result.features[0].properties.timezone.offset_DST}</p>
-                                                            <p>Offset DST Seconds: ${result.features[0].properties.timezone.offset_DST_seconds}</p>
-                                                            <p>Country: ${result.features[0].properties.country}</p>
-                                                            <p>Postcode: ${result.features[0].properties.postcode}</p>
-                                                            <p>City: ${result.features[0].properties.city}</p>
-                                                            </div> `;
-        }
-    })
-    .catch(error => console.log('error', error));
+    document.getElementById("add").style.display = "flex";
+    if(str.length == 0) {
+        document.getElementById("add").innerHTML = "Please enter a valid address";
+    } else {
+        fetch(`https://api.geoapify.com/v1/geocode/search?text=${str}&apiKey=a9212495cb8b43b6ab6eb2d331dd1a11`)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            document.getElementById("add").innerHTML = `<h1>Your Result</h1>
+                                                        <p>Name of Time Zone: ${result.features[0].properties.timezone.name}</p>
+                                                        <p>Lat: ${result.features[0].properties.lat}</p>
+                                                        <p>Long: ${result.features[0].properties.lon}</p>
+                                                        <p>Offset STD: ${result.features[0].properties.timezone.offset_STD}</p>
+                                                        <p>Offset STD Seconds: ${result.features[0].properties.timezone.offset_STD_seconds}</p>
+                                                        <p>Offset DST: ${result.features[0].properties.timezone.offset_DST}</p>
+                                                        <p>Offset DST Seconds: ${result.features[0].properties.timezone.offset_DST_seconds}</p>
+                                                        <p>Country: ${result.features[0].properties.country}</p>
+                                                        <p>Postcode: ${result.features[0].properties.postcode}</p>
+                                                        <p>City: ${result.features[0].properties.city}</p>`;
+        })
+        .catch(error => console.log('error', error));
+    }
 }
